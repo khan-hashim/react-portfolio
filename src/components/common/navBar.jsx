fc09b65ca39abe8a7dc08ce915a59e84e715cbc6
hashimak <hashimak@my.yorku.ca>
2024-07-17 23:43:57 -0400

    resume link

--- src/components/common/navBar.jsx
+++ src/components/common/navBar.jsx
@@ -39,15 +39,14 @@ const NavBar = (props) => {
 							>
 								<Link to="/projects">Projects</Link>
 							</li>
-							<li
-								className={
-									active === "articles"
-										? "nav-item active"
-										: "nav-item"
+							<li className={
+								active === "resume" 
+									? "nav-item active"
+									 : "nav-item"
 								}
 							>
-								<Link to="/articles">Resume</Link>
-							</li>
+								<a href="https://drive.google.com/file/d/1SgNTrimy9geBKuBcWr7RcSSq3bkInCqk/view?usp=drive_link" target="_blank" rel="noopener noreferrer">Resume</a>
+              				</li>
 							<li
 								className={
 									active === "contact"
